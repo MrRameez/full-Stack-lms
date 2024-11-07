b8b8d4f52306504f5e0d8f19f648ded20e503183
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2024-11-07 23:45:36 +0500

    first commit

--- src/app/(admin)/admin/courses/page.js
+++ src/app/(admin)/admin/courses/page.js
@@ -1,10 +1,18 @@
+import { CourseTable } from "@/components/DataTables/CourseTable";
+import { Button } from "@/components/ui/button";
 
 
 
 export default function Courses(){
     return(
-        <din className="min-h-screen p-10">
+        <div className="min-h-screen p-10">
+            <div className="flex justify-between my-2">
+
             <h1 className="text-4xl font-bold text-center">Courses</h1>
-        </din>
+            <Button>Add Courses</Button>
+            </div>
+            <CourseTable/>
+
+        </div>
     )
 }
