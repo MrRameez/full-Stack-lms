74500f78c347f3cb1d93bd689b1eb65cc12f7112
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2024-11-08 15:00:42 +0500

    first commit

--- src/app/(admin)/admin/courses/page.js
+++ src/app/(admin)/admin/courses/page.js
@@ -3,15 +3,14 @@ import { Button } from "@/components/ui/button";
 
 
 
-export default function Courses(){
-    return(
+export default function Courses() {
+    return (
         <div className="min-h-screen p-10">
             <div className="flex justify-between my-2">
-
-            <h1 className="text-4xl font-bold text-center">Courses</h1>
-            <Button>Add Courses</Button>
+                <h1 className="text-4xl font-bold text-center">Courses</h1>
+                <Button>Add Courses</Button>
             </div>
-            <CourseTable/>
+            <CourseTable />
 
         </div>
     )
