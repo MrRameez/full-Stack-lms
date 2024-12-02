f9eac8152ea448c96b0551771962f9dba65451a5
Rameez329732 <156177291+Rameez329732@users.noreply.github.com>
2024-12-03 00:42:03 +0500

    first commit

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,26 +1,42 @@
 import { connectDB } from "@/lib/dbConnect";
 import { UserModal } from "@/lib/modals/UserModal";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export async function POST(request) {
-    await connectDB()
-    const obj = await request.json();
-    console.log("object => ", obj);
+  await connectDB();
+  const obj = await request.json();
+  //check this user exist
+  const user = await UserModal.findOne({ email: obj.email });
+  if (user)
+    return Response.json(
+      { error: true, msg: "User Already Exist" },
+      { status: 403 }
+    );
 
-    // check this user exist
-    let user = await UserModal.findOne({email: obj.email });
+    // console.log("user =>", user);
+    
 
-    // if (user) {
-    //     return Response.json(
-    //         { error: true, msg: "User Already Exist" },
-    //         { status: 403}
-    //     )
-    //  }
-    console.log( "user =>",user);
-    
-    
-    return Response.json('User Post Request');
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(obj.password, saltRounds);
+  obj.password = hashedPassword;
+
+  let newUser = new UserModal({ ...obj });
+  newUser = await newUser.save();
+
+  const token = jwt.sign(
+    { id: newUser._id, role: newUser.role },
+    process.env.JWT_KEY
+  );
+
+  return Response.json({
+    error: false,
+    msg: "User Added Successfully",
+    user: newUser,
+    token,
+  });
 }
 
 export async function GET() {
-    return Response.json('User Get Request');
+  return Response.json("User Get Request");
 }
